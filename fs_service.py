4485cff978a99f4179d04b4f602c4bb8acf3a72f
kyotc <vlalqvlad@gmail.com>
2026-04-23 15:53:52 +0300

    Страдаю фигней

--- fs_service.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def safe_path(base: str, target: str) -> str | None:
-    full = os.path.abspath(target)
-    root = os.path.abspath(base)
-    if os.path.commonpath([root, full]) != root:
-        return None
-    return full
